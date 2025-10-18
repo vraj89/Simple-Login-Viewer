bd80d0fdb78e362dfaf39bfcd67b5ab30213a0e0
vraj89 <vrajrajeshbhaivaghela@gmail.com>
2025-10-18 23:47:12 +0530

    header is ready

---
+++ src/header.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { useContext } from 'react';
+import { mycontext } from './App.jsx';
+
+function Header(){
+   
+    const {name,setName,user,setUser}=useContext(mycontext)
+   
+    return(
+    
+        <header className='hwala' >
+            <div >My app</div>
+            <div className='user ? userdisp:userhide'> {name}</div>
+
+        </header>
+
+    )
+}
+
+export default Header;
